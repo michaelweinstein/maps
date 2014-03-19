fa1c50f65f5244ce753a069095504b5391dcf9df
michaelweinstein <michael_weinstein@brown.edu>
2014-03-19 14:00:49 -0400

    Conversion from LatLng to Vec2d working; only center node visible now (FOR TESTING ONLY). The rest of the nodes are off the screen (all node.x > dp.width). Ignore print lines for now relating to GUI launching, I will get rid of them later. Also ignore the center point being painted for conversion reference.

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ ARGS
 --gui /gpfs/main/home/miweinst/course/cs032/maps/src/edu/brown/cs032/miweinst/maps/unit_tests/test_data_files/ways.tsv /gpfs/main/home/miweinst/course/cs032/maps/src/edu/brown/cs032/miweinst/maps/unit_tests/test_data_files/nodes.tsv /gpfs/main/home/miweinst/course/cs032/maps/src/edu/brown/cs032/miweinst/maps/unit_tests/test_data_files/index.tsv
 
 TODO
+Direction of Ways for PathFinding??
 Way.hashCode()
+App REPL for Street names
+	BinaryHelper.findIntersection
 Still need to switch from Dijkstra's to A* in PathFinder! 
 Street and Cross-Street input in REPL
 	BinaryHelper.findIntersection
@@ -16,6 +19,7 @@ Create repOK() in BoundingBox to make sure corners not impossible
 
 
 WRITE TESTS
+BoundingBox.contains()
 BinaryHelper.findIntersection
 App.nearestNeighbor
 REPL for lat/lon and street/cross-street (System Tests)
---
+++ src/edu/brown/cs032/miweinst/maps/unit_tests/GUITest.java
@@ -0,0 +1,49 @@
+package edu.brown.cs032.miweinst.maps.unit_tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import edu.brown.cs032.miweinst.maps.App;
+import edu.brown.cs032.miweinst.maps.maps.FileProcessor;
+import edu.brown.cs032.miweinst.maps.maps.GUIInfo;
+import edu.brown.cs032.miweinst.maps.maps.MapsFile;
+import edu.brown.cs032.miweinst.maps.maps.path.BinaryHelper;
+import edu.brown.cs032.miweinst.maps.util.BoundingBox;
+import edu.brown.cs032.miweinst.maps.util.LatLng;
+import edu.brown.cs032.miweinst.maps.util.Vec2d;
+
+public class GUITest {
+	
+	private String waysPath = "/Users/michaelweinstein/Desktop/Work/cs032_software_engineering/maps/src/edu/brown/cs032/miweinst/maps/unit_tests/test_data_files/ways.tsv";
+	private String nodesPath = "/Users/michaelweinstein/Desktop/Work/cs032_software_engineering/maps/src/edu/brown/cs032/miweinst/maps/unit_tests/test_data_files/nodes.tsv";
+	private String indexPath = "/Users/michaelweinstein/Desktop/Work/cs032_software_engineering/maps/src/edu/brown/cs032/miweinst/maps/unit_tests/test_data_files/index.tsv";
+	
+	@Test
+	public void centerTest()
+	{
+		try {
+			//make FileProcessor and set BinaryHelper files
+			MapsFile ways = new MapsFile(waysPath);
+			MapsFile nodes = new MapsFile(nodesPath);
+			MapsFile index = new MapsFile(indexPath);
+			BinaryHelper.setFiles(ways, nodes, index);
+			FileProcessor fp = new FileProcessor(nodes, index, ways);
+			//Create BoundingBox and try to convert center LatLng to XY
+			BoundingBox box = new BoundingBox(new LatLng(40.2720844,	-73.7175365), new LatLng(40.1615135, -73.6937661));
+			LatLng c = box.getCenter();
+			GUIInfo gui = new GUIInfo(fp, box);
+			Vec2d screenCenter = gui.convertToScreen(c);
+			Vec2d dpDim = gui.getDimensions();
+			Vec2d dpCenter = new Vec2d(dpDim.x/2, dpDim.y/2);
+///
+			System.out.println("dpCenter: " + dpCenter);
+			System.out.println("convertToScreen(c): " + screenCenter.toString());
+		} catch (IOException e) {
+			assertTrue(false);
+		}
+	}
+
+}
